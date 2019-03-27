0b6805ebc47a79a75406b739d339545c686ed7c8
ZequnSong <36482357+ZequnSong@users.noreply.github.com>
2019-03-27 16:17:15 -0400

    Add files via upload

---
+++ Queue/PriorityQueue.java
@@ -0,0 +1,61 @@
+package com.queue;
+
+public class PriorityQueue {
+    private int maxSize;
+    private int[] priQueArray;
+    private int nItems;
+
+    public PriorityQueue(int maxSize){
+        this.maxSize = maxSize;
+        priQueArray = new int[maxSize];
+        nItems = 0;
+    }
+
+    /**
+     * insert new item into the queue
+     * @param value
+     */
+    public void insert(int value){
+        if(nItems == 0){
+            priQueArray[nItems++] = value;
+        }else{
+            //insertion-sort, the largest value is on the top of queue
+            int j = nItems -1;
+            while(j >=0 && value < priQueArray[j]){
+                priQueArray[j+1] = priQueArray[j];
+                j--;
+            }
+            priQueArray[j+1] = value;
+            nItems++;
+        }
+    }
+
+    /**
+     * delete the biggest value
+     * -1 means deleted because int[]array cannot be null
+     * @return
+     */
+    public int remove(){
+        int k = nItems -1;
+        int value = priQueArray[k];
+        priQueArray[k] = -1;
+        nItems--;
+        return value;
+    }
+
+    /**
+     * find the highest priority value
+     * @return
+     */
+    public int highestPri(){
+        return priQueArray[nItems-1];
+    }
+
+    public boolean isEmpty(){
+        return (nItems == 0);
+    }
+
+    public boolean isFull(){
+        return (nItems == maxSize);
+    }
+}
